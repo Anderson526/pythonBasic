cdb4c6230b4ad35b8acc6d6dfa64a87ba7bddfda
Ander <1megaword1@gmail.com>
2023-11-16 09:25:42 -0500

    new commit int var and print command

---
+++ pythonEx/ex1.py
@@ -0,0 +1,16 @@
+#Autor: Anderson Chila 
+#tipos de datos en numeros
+
+
+decimal= 8
+binario=0b1101
+octal=0o11
+hexadecimal=0xc
+
+
+#output de una variable
+
+print(f"numero en decimal:{decimal}")
+print("numero en binario:"+str(binario))
+print(f"numero en octal: {octal}")
+print("nuemro en hexadecimal:"+str(hexadecimal))
