465d3cd02c74b1971e7f468fa8f7eb20cba15dc6
Ander <1megaword1@gmail.com>
2023-12-04 11:47:50 -0500

    condition pt 2

--- pythonEx/ex4.py
+++ pythonEx/ex4.py
@@ -3,3 +3,12 @@ if x > 29:
     print(str(x) +" es mayor")
 else:
     print(str(x) + " es menor")
+
+
+    temperatura = 25
+lluvia = False
+
+if temperatura > 20 and not lluvia:
+    print("Es un buen día para salir.")
+elif temperatura <= 20 or lluvia:
+    print("Mejor quédate en casa.")
